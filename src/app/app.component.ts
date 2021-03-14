542eca53e1e567ae78de446a5da98d9ace28cfec
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2021-03-15 01:21:02 +0200

    Creating the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomSentence = lorem.sentence();
+  inputText = '';
+  generateNewSentenece() {
+    this.randomSentence = lorem.sentence();
+  }
+  onTyping(value: string) {
+    console.log(value);
+    this.inputText = value;
+  }
+  evaluateConvenientColor(letter: string, index: number) {
+    if (!this.inputText[index]) {
+      return 'grey';
+    }
+    return letter == this.inputText[index] ? 'green' : 'red';
+  }
 }
